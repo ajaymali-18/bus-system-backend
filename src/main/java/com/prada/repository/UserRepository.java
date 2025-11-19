757983004b3750f192b2895ec7d2626d31fe0099
Ajay Mali <work.ajay27@gmail.com>
2025-11-19 20:15:59 +0530

    Create User Repo

---
+++ src/main/java/com/prada/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.prada.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//User → Which table you want to interact with
+//This repository will work on the User table.”
+//Long → What is the datatype of your primary key
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	boolean existsByEmail(String email);
+
+	User findByEmail(String email);
+}
+
+//Used to check duplicate emails
+// Used for login validation
